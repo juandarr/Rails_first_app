5d44b04c76d39f1d17c8d62bbe897dac16b89701
Juan Rios <juandarr@gmail.com>
2020-08-12 17:02:11 -0500

    add: new helper class to format time as an example of business logic outside of the view

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+    random_names = ["Juan","Sebas","Julian"]
+    @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||= 0
+    @times_displayed += 1
   end
   def goodbye
   end
